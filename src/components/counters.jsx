ca6e51ceeaf2f48ecb42b1ba0c64109b07ce1a7b
saurabhh-svg <singhsaurabh2901@gmail.com>
2023-01-01 19:29:52 +0530

    final

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,41 +2,11 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-      { id: 5, value: 0 },
-    ],
-  };
-
-  handleIncrement = (counter) => {
-    //console.log(counter);
-     const counters = [...this.state.counters];
-  };
-
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map((c) => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({ counters });
-  };
-
   render() {
+    const { onReset, onDelete, onIncrement, counters } = this.props;
     return (
       <div>
-        <button
-          onClick={this.handleReset}
-          className="btn btn-primary btn-sm mg-2"
-        >
+        <button onClick={onReset} className="btn btn-primary btn-sm mg-2">
           {" "}
           Reset
         </button>
@@ -44,8 +14,8 @@ class counters extends Component {
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
             counter={counter}
           />
         ))}
--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
-import Counters from "./components/counters";
+import Navbar from "./components/navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <>
-      <Counters />
+      <App />
     </>
   </React.StrictMode>
 );
